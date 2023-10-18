d95a31f9a1999cbfbc1ad3025fde589a80b9cb77
Brandon Schnurbusch <b33blbroxx@gmail.com>
2023-10-17 20:54:40 -0500

    Got setup done, players rendering to page, and delete working

---
+++ api/playerData.js
@@ -0,0 +1,64 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getPlayers = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/players.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.warn(data);
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const deletePlayer = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/players/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const createPlayer = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/players.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updatePlayer = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/players/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getPlayers,
+  deletePlayer,
+  createPlayer,
+  updatePlayer,
+};
